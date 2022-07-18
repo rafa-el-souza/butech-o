8ae1553fe0383b1b4fe42368dd130300e7efad52
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-18 06:15:59 -0300

    feat: add model class interface for complete crud endpoints

---
+++ src/app/helpers/types/model.interface.ts
@@ -0,0 +1,8 @@
+export interface CRUDModelInterface<
+  Output, CreateInput, UpdateInput, DeleteInput
+> {
+  create(obj: CreateInput): Promise<Output>;
+  read(): Promise<Array<Output>>;
+  update(obj: UpdateInput): Promise<Output | null>;
+  delete(obj: DeleteInput): Promise<Output | null>;
+}
