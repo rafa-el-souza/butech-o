9384fd9c08b582f7dad466c90e77140d27669c8c
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-18 06:24:44 -0300

    feat: add interfaces for category inputs

---
+++ src/app/helpers/types/index.ts
@@ -0,0 +1,5 @@
+export * from './category.interface';
+export * from './controller.interface';
+export * from './model.interface';
+export * from './product.interface';
+export * from './service.interface';
