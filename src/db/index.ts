b8b4658087cbbb3213ac8b5f5a9de5e247a0e494
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-18 06:32:59 -0300

    chore: barrel db files

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,3 @@
-export * from './models/category.mongoose.model';
 export * from './connection';
-export * from './models/product.mongoose.model';
+export * from './helpers';
+export * from './models';
