8ee394ba6833092f6d2af2c723b20565754cd803
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-18 11:16:31 -0300

    feat: add category router to server

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,9 +1,11 @@
 import Api from './api';
-import { Product } from './helpers';
+import { Category, Product } from './helpers';
 
 const server = new Api();
 
 server.addRouter(Product().router, '/products');
+
+server.addRouter(Category().router, '/categories');
 
 server.handleErrors();
 
