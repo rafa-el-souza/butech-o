b1a04f331ea8927c02df022e185a73d969a9e64b
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-20 12:09:02 -0300

    refactor: add inventory to product schema

--- src/app/helpers/validation/product.validation.ts
+++ src/app/helpers/validation/product.validation.ts
@@ -2,6 +2,24 @@ import { z } from 'zod';
 
 import { createCategoryZodSchema } from './category.validation';
 
+export const inventoryZodSchema = z.object({
+  quantidade: z.number({
+    required_error: 'Inventory quantity is required',
+    invalid_type_error: 'Inventory quantity must be a number',
+  }),
+  precoDeVenda: z.number({
+    required_error: 'Inventory precoDeCompra is required',
+    invalid_type_error: 'Inventory precoDeCompra must be a number',
+  }),
+  precoDeCompra: z.number({
+    required_error: 'Inventory precoDeVenda is required',
+    invalid_type_error: 'Inventory precoDeVenda must be a number',
+  }),
+}, {
+  required_error: 'Inventory is required',
+  invalid_type_error: 'Inventory must be an must have quantidade, precoDeVenda and precoDeCompra',
+});
+
 export const createProductZodSchema = z.object({
   nome: z.string({
     required_error: 'Product name is required',
@@ -9,21 +27,20 @@ export const createProductZodSchema = z.object({
   }).min(3, { message: 'Product name must be 3 or more characters long' }),
 
   descricao: z.string({
-    required_error: 'Product name is required',
+    required_error: 'Product description is required',
     invalid_type_error: 'Product description must be a string',
   }).min(3, { message: 'Product description must be 3 or more characters long' }),
 
   imagens: z.array(z.string({
-    invalid_type_error: 'Imagens must be an array of strings',
-  })).max(5),
+    invalid_type_error: 'Images must be an array of strings',
+  }), { required_error: 'Images are required' }).max(5),
 
-  estoque: z.object({
-    quantidade: z.number(),
-    precoDeVenda: z.number(),
-    precoDeCompra: z.number(),
+  estoque: inventoryZodSchema,
+
+  categorias: z.array(createCategoryZodSchema, {
+    required_error: 'Categories is required',
+    invalid_type_error: 'Inventory must be an must have quantidade, precoDeVenda and precoDeCompra',
   }),
-
-  categorias: z.array(createCategoryZodSchema),
 });
 
 export const updateProductZodSchema = z.object({
@@ -49,5 +66,7 @@ export const updateProductZodSchema = z.object({
 
   categorias: z.optional(z.array(createCategoryZodSchema)),
 });
+
+export type InventoryInterface = z.infer<typeof inventoryZodSchema>
 
 export type ProductInterface = z.infer<typeof createProductZodSchema>;
--- src/db/models/product.mongoose.model.ts
+++ src/db/models/product.mongoose.model.ts
@@ -1,16 +1,26 @@
 import { Schema, model as createModel, Document } from 'mongoose';
 
 import { categorySchema } from './category.mongoose.model';
-import { ProductInterface } from '../../app/helpers/validation';
+import { InventoryInterface, ProductInterface } from '../../app/helpers/validation';
 
 export interface ProductDocument extends ProductInterface, Document { }
+
+export interface InventoryDocument extends InventoryInterface, Document { }
+
+export const inventorySchema = new Schema<InventoryDocument>({
+  quantidade: { type: Number, required: true },
+  precoDeCompra: { type: Number, required: true },
+  precoDeVenda: { type: Number, required: true },
+});
 
 export const productSchema = new Schema<ProductDocument>({
   nome: { type: String, required: true },
   descricao: { type: String, required: true },
   imagens: { type: [String], required: true },
-  estoque: { type: String, required: true },
+  estoque: { type: inventorySchema, required: true },
   categorias: { type: [categorySchema], required: true },
 }, { versionKey: false, timestamps: true });
+
+export const inventoryMongooseModel = createModel('Inventory', productSchema);
 
 export const productMongooseModel = createModel('Product', productSchema);
