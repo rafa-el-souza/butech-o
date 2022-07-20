1b17535555c322d646eb2dc57784fa8504027d60
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-20 11:51:20 -0300

    refactor: set crud model abstract class to inherit from new generic model

--- src/app/models/crud.model.ts
+++ src/app/models/crud.model.ts
@@ -1,24 +1,26 @@
 import { Model as MongooseModel, Document } from 'mongoose';
 
 import { CRUDModelInterface } from '../helpers/types';
+import { GenericModel } from './generic.model';
 
 export abstract class CRUDModel<
   Output, CreateInput, UpdateInput, DeleteInput
 >
-implements CRUDModelInterface<
+  extends GenericModel<Output>
+  implements CRUDModelInterface<
   Output, CreateInput, UpdateInput, DeleteInput
-> {
+  > {
   constructor(
-    protected dataBaseModel: MongooseModel<Output & Document>,
-  ) { this.dataBaseModel = dataBaseModel; }
+      protected dataBaseModel: MongooseModel<Output & Document>,
+  ) { super(dataBaseModel); }
 
-  abstract create(obj: CreateInput): Promise<Output>;
+    abstract create(obj: CreateInput): Promise<Output>;
 
-  abstract read(): Promise<Array<Output>>;
+    abstract read(): Promise<Array<Output>>;
 
-  abstract update(obj: UpdateInput): Promise<Output | null>;
+    abstract update(obj: UpdateInput): Promise<Output | null>;
 
-  abstract delete(obj: DeleteInput): Promise<Output | null>;
+    abstract delete(obj: DeleteInput): Promise<Output | null>;
 }
 
 export default CRUDModel;
