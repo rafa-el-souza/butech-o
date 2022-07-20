cd63f5d8161f3ab4d70fe056bf467b2ffc6fa722
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-20 13:25:47 -0300

    feat: add worker login model class

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,3 +1,6 @@
 export * from './category.model';
 export * from './crud.model';
+export * from './generic.model';
+export * from './login.model';
 export * from './product.model';
+export * from './worker.login.model';
