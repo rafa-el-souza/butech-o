9af411922354d1ec149e06ed4af5eaa9fe5c53ac
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-20 13:32:04 -0300

    feat: add worker login controller class

--- src/app/controllers/index.ts
+++ src/app/controllers/index.ts
@@ -1,4 +1,6 @@
 export * from './category.controller';
 export * from './crud.controller';
 export * from './generic.controller';
+export * from './login.controller';
 export * from './product.controller';
+export * from './worker.login.controller';
