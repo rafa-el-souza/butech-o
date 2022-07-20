5792480127f880ef6fcfcbf05d129a8d01408665
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-20 13:28:39 -0300

    feat: add worker login service class

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,4 +1,6 @@
 export * from './category.service';
 export * from './crud.service';
 export * from './generic.service';
+export * from './login.service';
 export * from './product.service';
+export * from './worker.login.service';
