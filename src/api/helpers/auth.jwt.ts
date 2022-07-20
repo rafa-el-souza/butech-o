4c50daec08e3b8a466e2c4fe3eba287802f47a89
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-20 12:07:17 -0300

    fix: auth middleware export

--- src/api/helpers/auth.jwt.ts
+++ src/api/helpers/auth.jwt.ts
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import { sign, verify, JwtPayload } from 'jsonwebtoken';
 
-export default {
+export const jwt = {
   auth: async (token: string): Promise<JwtPayload & { role: string }> => {
     const secret = process.env.JWT_SECRET;
     const options = {
@@ -9,7 +9,7 @@ export default {
       maxAge: process.env.JWT_MAX_AGE,
     };
     return new Promise((resolve, reject) => {
-      jwt.verify(token, secret as string, options as {}, (err, decoded) => {
+      verify(token, secret as string, options as {}, (err, decoded) => {
         if (err || !decoded) return reject(err);
         resolve(decoded as JwtPayload & { role: string });
       });
@@ -22,7 +22,7 @@ export default {
       expiresIn: process.env.JWT_MAX_AGE,
     };
     return new Promise((resolve, reject) => {
-      jwt.sign(payload, secret as string, options as {}, (err, token) => {
+      sign(payload, secret as string, options as {}, (err, token) => {
         if (err || !token) return reject(err);
         resolve(token);
       });
