24eb6e9da28d3cf3f11f1c8ca0d73fa3a5bb59d7
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-21 09:52:12 -0300

    feat: add worker controller

--- src/app/controllers/index.ts
+++ src/app/controllers/index.ts
@@ -3,4 +3,5 @@ export * from './crud.controller';
 export * from './generic.controller';
 export * from './login.controller';
 export * from './product.controller';
+export * from './worker.controller';
 export * from './worker.login.controller';
