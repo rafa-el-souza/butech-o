52d8f3eafebcf1008c9752c1b597a92cb94e4caf
Rafa Souza <rafasouza.developer@gmail.com>
2022-07-21 09:57:30 -0300

    Revert "feat: add worker router to server"
    This reverts commit 7741a9f955593e761ba1ee77d7bbd6155a74a758.

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,17 +1,13 @@
 import Api from './api';
-import {
-  Category, Product, Worker, WorkerLogin,
-} from './helpers';
+import { Category, Product, WorkerLogin } from './helpers';
 
 const server = new Api();
 
-server.addRouter(WorkerLogin().router, '/login/worker');
+server.addRouter(WorkerLogin().router, '/worker');
 
 server.addRouter(Product().router, '/products');
 
 server.addRouter(Category().router, '/categories');
-
-server.addRouter(Worker().router, '/worker');
 
 server.handleErrors();
 
