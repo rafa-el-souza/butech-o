7741a9f955593e761ba1ee77d7bbd6155a74a758
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-21 09:52:43 -0300

    feat: add worker router to server

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,13 +1,17 @@
 import Api from './api';
-import { Category, Product, WorkerLogin } from './helpers';
+import {
+  Category, Product, Worker, WorkerLogin,
+} from './helpers';
 
 const server = new Api();
 
-server.addRouter(WorkerLogin().router, '/worker');
+server.addRouter(WorkerLogin().router, '/login/worker');
 
 server.addRouter(Product().router, '/products');
 
 server.addRouter(Category().router, '/categories');
+
+server.addRouter(Worker().router, '/worker');
 
 server.handleErrors();
 
